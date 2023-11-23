c1b9b04d1c2fffcf65b78874248428a797b1ff1d
Björn Einert <Bj.Einert@gmail.com>
2023-11-24 00:17:10 +0100

    v1.1

--- popup.js
+++ popup.js
@@ -1,5 +1,12 @@
-document.getElementById('openTab').addEventListener('click', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.create({ url: tabs[0].url });
-    });
-  });
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  if (tabs[0].url.includes('youtube.com/shorts')) {
+    const shortId = tabs[0].url.split('/').pop();
+    const video = document.querySelector('video');
+    const videoUrl = `https://www.youtube.com/watch?v=${shortId}`;
+    chrome.tabs.create({ url: videoUrl, index: tabs[0].index + 1, active: false });
+  }
+    if (video) {
+      video.pause();
+  }
+});
